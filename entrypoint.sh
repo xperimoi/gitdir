f85b3a7d23ac5ed21eda7e539a0044ed6b98e608
murray <experimoi@gmail.com>
2022-12-12 19:23:20 -0500

    tst msg 19

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,4 @@ INPUT_SCHEMA=schema.yaml
 
 options=(--schema "$INPUT_SCHEMA")
 
-# Enable globstar so ** globs recursively
-#shopt -s globstar
-# Use the current directory by default
-#for file in ${INPUT_FILES:-.}; do
-#    echo " INFO - validating $file"
-#    yamale "${options[@]}" --data-file $file
-#done
 yamale "${options[@]}"  file1.yaml
